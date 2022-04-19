bfa0d66a00754c072bd025ccd5cc5f42dba6381c
Ryan Mburu <mbururyan31@gmail.com>
2022-04-19 17:10:49 +0300

    added two info cards on dashboard

--- dashboard.py
+++ dashboard.py
@@ -16,10 +16,51 @@ import numpy as np
 # Application initialization
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+# Information Cards
+
+card_age = dbc.Card(
+    [
+        dbc.CardHeader('Most Affected Age Group : '),
+        dbc.CardBody(
+            [
+                html.P('50 - 55 Years')
+            ]
+        )
+    ]
+)
+
+card_gender = dbc.Card(
+    [
+        dbc.CardHeader('Most Affected Gender: '),
+        dbc.CardBody(
+            [
+                html.P('Female')
+            ]
+        )
+    ]
+)
+
+info_cards = dbc.CardDeck(
+    [
+        dbc.Col(card_age, width = 'auto'),
+        dbc.Col(card_gender, width = 'auto')
+    ],
+)
+
 app.layout = dbc.Container(
     [
         html.H1('HYPOTHYROID DISEASE ANALYSIS'),
-        html.Hr()
+        html.Hr(),
+        dbc.Row(
+            [
+                html.Div(
+                    [
+                        info_cards
+                    ]
+                ),
+            ]
+        ),
+        html.Br(),
     ]
 )
 
